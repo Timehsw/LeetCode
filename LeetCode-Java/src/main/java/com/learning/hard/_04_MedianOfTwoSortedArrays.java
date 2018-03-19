fc6e664e5ea1c6e8adbaf42c57d0a3067f59cda9
HswTime <hsw.time@gmail.com>
2018-03-19 18:31:08 +0800

    add medium and hard folder

---
+++ LeetCode-Java/src/main/java/com/learning/hard/_04_MedianOfTwoSortedArrays.java
@@ -0,0 +1,8 @@
+package com.learning.hard;
+
+/**
+ * Created by hushiwei on 2018/3/19.
+ * desc :
+ */
+public class _04_MedianOfTwoSortedArrays {
+}
---
+++ LeetCode-Java/src/main/java/com/learning/medium/_02_AddTwoNumbers.java
@@ -0,0 +1,8 @@
+package com.learning.medium;
+
+/**
+ * Created by hushiwei on 2018/3/19.
+ * desc :
+ */
+public class _02_AddTwoNumbers {
+}
