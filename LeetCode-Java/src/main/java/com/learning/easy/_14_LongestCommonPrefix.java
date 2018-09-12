874f4dd840c8b6f15b26a4e4b0851f3a3da14052
HswTime <hsw.time@gmail.com>
2018-09-12 10:49:41 +0800

    add _14_LongestCommonPrefix solution

---
+++ LeetCode-Java/src/main/java/com/learning/easy/_14_LongestCommonPrefix.java
@@ -0,0 +1,20 @@
+package com.learning.easy;
+
+/**
+ * Created by hushiwei on 2018/4/4.
+ * desc : 最长公共前缀
+ * <p>
+ * Write a function to find the longest common prefix string amongst an array of strings.
+ * 编写一个函数来查找字符串数组中最长的公共前缀字符串。
+ */
+public class _14_LongestCommonPrefix {
+    public String longestCommonPrefix(String[] strs) {
+        return "";
+    }
+
+    public static void main(String[] args) {
+        String[] arr = {"a"};
+        new _14_LongestCommonPrefix().longestCommonPrefix(arr);
+    }
+
+}
