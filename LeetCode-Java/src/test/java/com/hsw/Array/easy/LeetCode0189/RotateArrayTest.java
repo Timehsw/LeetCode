09af94ff9dfa95b7751b6ca3546d5da64f15b344
HswTime <hsw.time@gmail.com>
2019-03-01 19:21:55 +0800

    add leetcode 189 RotateArray

---
+++ LeetCode-Java/src/test/java/com/hsw/Array/easy/LeetCode0189/RotateArrayTest.java
@@ -0,0 +1,19 @@
+package com.hsw.Array.easy.LeetCode0189;
+
+import org.junit.Test;
+
+/**
+ * Created by hushiwei on 2019-03-01.
+ * desc :
+ */
+public class RotateArrayTest {
+    @Test
+    public void testRoate() {
+        RotateArray t = new RotateArray();
+        t.rotate(new int[]{1, 2, 3, 4, 5, 6, 7}, 3);
+        t.rotate_1(new int[]{1, 2, 3, 4, 5, 6, 7}, 3);
+        t.rotate_2(new int[]{1, 2, 3, 4, 5, 6, 7}, 3);
+        t.rotate_3(new int[]{1, 2, 3, 4, 5, 6, 7}, 3);
+
+    }
+}
